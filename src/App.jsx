0ea6e275af9f888efc31d66eeffb71dc93052160
Aroundhead <a219207415@unison.mx>
2024-02-20 18:28:05 -0700

    destructuring props 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
 import React from 'react';
-import { sayHello } from './FunctionJS';
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import CoreConcept from './components/CoreConcept.jsx';
 import { CORE_CONCEPTS } from './data.js';
-const reactDescriptions = ['Fundamental ', 'Crucial ','Core   '];
+import Header from './components/Header.jsx';
 
 
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header(){
-  return (<header>
-    <img src={reactImg} alt= "Stylized atom"/>
-    <h1>React Essentials</h1>
-    <p>
-    {reactDescriptions[genRandomInt(2)]} Fundamental React concepts you will need for almost any app you are going to build! 
-    {"\n"+ sayHello()}
-    </p>
-  </header>
-  );
- 
-}
 
-function CoreConcept(image, description, title){
-   return (
-    <li>
-      <img src={image} alt ={title}/>   
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-      );
-}
 
 function App() {
   return (
